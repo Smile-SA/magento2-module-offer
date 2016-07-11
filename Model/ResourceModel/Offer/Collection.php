43fa70aa30f76f0caa1d770e16601363a81a96f2
Aurélien FOUCRET <aurelien.foucret@smile.fr>
2016-07-11 14:22:45 +0200

    Append attribute set filter to collection

--- Model/ResourceModel/Offer/Collection.php
+++ Model/ResourceModel/Offer/Collection.php
@@ -7,10 +7,28 @@ namespace Smile\Offer\Model\ResourceModel\Offer;
 
 
 use \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
-
+use Smile\Seller\Api\Data\SellerInterface;
 
 class Collection extends AbstractCollection
 {
+    /**
+     * @var \Magento\Framework\EntityManager\MetadataPool $metadataPool
+     */
+    private $metadataPool;
+
+    public function __construct(
+        \Magento\Framework\Data\Collection\EntityFactoryInterface $entityFactory,
+        \Psr\Log\LoggerInterface $logger,
+        \Magento\Framework\Data\Collection\Db\FetchStrategyInterface $fetchStrategy,
+        \Magento\Framework\Event\ManagerInterface $eventManager,
+        \Magento\Framework\EntityManager\MetadataPool $metadataPool,
+        \Magento\Framework\DB\Adapter\AdapterInterface $connection = null,
+        \Magento\Framework\Model\ResourceModel\Db\AbstractDb $resource = null
+    ) {
+        parent::__construct($entityFactory, $logger, $fetchStrategy, $eventManager, $connection, $resource);
+        $this->metadataPool = $metadataPool;
+    }
+
     /**
      * Define resource model
      *
@@ -19,5 +37,11 @@ class Collection extends AbstractCollection
     protected function _construct()
     {
         $this->_init('Smile\Offer\Model\Offer', 'Smile\Offer\Model\ResourceModel\Offer');
+    }
+
+    public function addSellerTypeFilter($sellerType)
+    {
+        $sellerMetadata = $this->metadataPool->getMetadata(SellerInterface::ENTITY);
+        var_dump($sellerMetadata);
     }
 }
---
+++ Model/ResourceModel/Offer/Grid/Collection.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Smile\Offer\Model\ResourceModel\Offer\Grid;
+
+
+
+class Collection extends \Smile\Offer\Model\ResourceModel\Offer\Collection
+{
+
+}

d1bdf29be959d200f16dff8e78a8e8504e8ca2eb
Romain Ruaud <romain.ruaud@smile.fr>
2016-07-12 17:24:24 +0200

    Added Locator interface and its implementation for offers.

---
+++ Model/Locator/LocatorInterface.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * DISCLAIMER
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future.
+ *
+ * @category  Smile
+ * @package   Smile\Offer
+ * @author    Romain Ruaud <romain.ruaud@smile.fr>
+ * @copyright 2016 Smile
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+namespace Smile\Offer\Model\Locator;
+
+use Smile\Offer\Api\Data\OfferInterface;
+
+/**
+ * Offer Locator Interface
+ *
+ * @category Smile
+ * @package  Smile\Offer
+ * @author   Romain Ruaud <romain.ruaud@smile.fr>
+ */
+interface LocatorInterface
+{
+    /**
+     * @return OfferInterface
+     */
+    public function getOffer();
+}
---
+++ Model/Locator/RegistryLocator.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * DISCLAIMER
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future.
+ *
+ * @category  Smile
+ * @package   Smile\Offer
+ * @author    Romain Ruaud <romain.ruaud@smile.fr>
+ * @copyright 2016 Smile
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+namespace Smile\Offer\Model\Locator;
+
+use Magento\Framework\Exception\NotFoundException;
+use Magento\Framework\Registry;
+use Smile\Offer\Api\Data\OfferInterface;
+
+/**
+ * Registry Locator for offers
+ *
+ * @category Smile
+ * @package  Smile\Offer
+ * @author   Romain Ruaud <romain.ruaud@smile.fr>
+ */
+class RegistryLocator implements LocatorInterface
+{
+    /**
+     * @var Registry
+     */
+    private $registry;
+
+    /**
+     * @var OfferInterface
+     */
+    private $offer;
+
+    /**
+     * @param Registry $registry The application registry
+     */
+    public function __construct(Registry $registry)
+    {
+        $this->registry = $registry;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @throws NotFoundException
+     */
+    public function getOffer()
+    {
+        if (null !== $this->offer) {
+            return $this->offer;
+        }
+
+        if ($this->registry->registry('current_offer')) {
+            return $this->offer = $this->registry->registry('current_offer');
+        }
+
+        throw new NotFoundException(__('Offer was not registered'));
+    }
+}
